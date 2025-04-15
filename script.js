418d30ca59ff71b662d98400c13d9ff30b3dcad6
Hager Amr <hageramr320@gmail.com>
2025-04-15 13:48:12 +0200

    implement check button logic and reset function

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-const secretNumber = Math.trunc(Math.random() * 20);
-console.log(secretNumber);
+const checkBtn = document.querySelector(".check");
+let secretNumber = Math.trunc(Math.random() * 20);
+let number = document.querySelector(".number");
 let message = document.querySelector(".message");
 let score = 20;
-document.querySelector(".check").addEventListener("click", function () {
+let body = document.querySelector("body");
+const againBtn = document.querySelector(".again");
+// for testing
+console.log(secretNumber);
+
+function checkScore() {
+  if (score > 1) {
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else {
+    message.textContent = "You Lose The Game :(";
+  }
+}
+checkBtn.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
+  //   when there is no input
   if (!guess) {
     message.textContent = "No Number!!";
-  } else if (guess > secretNumber) {
-    message.textContent = "Too High !!";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess < secretNumber) {
-    message.textContent = "Too LoW !!";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess === secretNumber) {
-    message.textContent = "🎉🎉 Congratulations! 🎉🎉";
-    document.querySelector(".number").textContent = secretNumber;
   }
+  //   when guess is high
+  else if (guess > secretNumber) {
+    message.textContent = "📈 Too High !!";
+    checkScore();
+  } // when guess is low
+  else if (guess < secretNumber) {
+    message.textContent = "📉 Too LoW !!";
+    checkScore();
+  } // when guess === secret number
+  else if (guess === secretNumber) {
+    message.textContent = "🎉🎉 Correct Number, Congratulations! 🎉🎉";
+    number.textContent = secretNumber;
+    body.style.backgroundColor = "#60b347";
+    number.style.width = "30rem";
+  }
+});
+
+againBtn.addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20);
+  message.textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = score;
+  number.textContent = "?";
+  body.style.backgroundColor = "#333";
+  number.style.width = "15rem";
 });
